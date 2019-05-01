d71e1f0befc8d15d2a17f83fbcc9bdd86477900e
tennercheung <tennercheung@gmail.com>
2019-05-01 19:34:17 -0400

    added v1.0 of working lib

---
+++ examples/Bubble_Display_Text_NSA1166/Bubble_Display_Text_NSA1166.ino
@@ -0,0 +1,280 @@
+/*
+ March 6, 2014
+ Spark Fun Electronics
+ Nathan Seidle
+ Updates by Joel Bartlett, followed by Tenner C.
+ 
+ This code is originally based Dean Reading's Library deanreading@hotmail.com
+ http://arduino.cc/playground/Main/SevenSegmentLibrary
+ 
+The example shows you how to display characters to the bubble display. Since this is a 7-segment display
+and not an alpha-numeric display, there are numerous characters that cannot be displayed. The table below 
+should give you an idea which ones can. 
+ 
+ 
+ Pinout for HP Bubble Display:
+ 1:  Cathode 1
+ 2:  Anode E
+ 3:  Anode C
+ 4:  Cathode 3
+ 5:  Anode dp
+ 6:  Cathode 4
+ 7:  Anode G
+ 8:  Anode D
+ 9:  Anode F
+ 10: Cathode 2
+ 11: Anode B
+ 12: Anode A
+ 
+ Here is the character map found in the SevSeg.h file for reference 
+ as to which characters can be displayed and which can't. 
+ 
+ //  ABCDEFG  Segments
+  0b1111110, // 0
+  0b0110000, // 1
+  0b1101101, // 2
+  0b1111001, // 3
+  0b0110011, // 4
+  0b1011011, // 5
+  0b1011111, // 6
+  0b1110000, // 7
+  0b1111111, // 8
+  0b1111011, // 9
+  0b1110111, // 10  "A"
+  0b0011111, // 11  "B"
+  0b1001110, // 12  "C"
+  0b0111101, // 13  "D"
+  0b1001111, // 14  "E"
+  0b1000111, // 15  "F"
+  0b0000000, // 16  NO DISPLAY
+  0b0000000, // 17  NO DISPLAY
+  0b0000000, // 18  NO DISPLAY
+  0b0000000, // 19  NO DISPLAY
+  0b0000000, // 20  NO DISPLAY
+  0b0000000, // 21  NO DISPLAY
+  0b0000000, // 22  NO DISPLAY
+  0b0000000, // 23  NO DISPLAY
+  0b0000000, // 24  NO DISPLAY
+  0b0000000, // 25  NO DISPLAY
+  0b0000000, // 26  NO DISPLAY
+  0b0000000, // 27  NO DISPLAY
+  0b0000000, // 28  NO DISPLAY
+  0b0000000, // 29  NO DISPLAY
+  0b0000000, // 30  NO DISPLAY
+  0b0000000, // 31  NO DISPLAY
+  0b0000000, // 32 ' '
+  0b0000000, // 33 '!'  NO DISPLAY
+  0b0100010, // 34 '"'
+  0b0000000, // 35 '#'  NO DISPLAY
+  0b0000000, // 36 '$'  NO DISPLAY
+  0b0000000, // 37 '%'  NO DISPLAY
+  0b0000000, // 38 '&'  NO DISPLAY
+  0b0100000, // 39 '''
+  0b1001110, // 40 '('
+  0b1111000, // 41 ')'
+  0b0000000, // 42 '*'  NO DISPLAY
+  0b0000000, // 43 '+'  NO DISPLAY
+  0b0000100, // 44 ','
+  0b0000001, // 45 '-'
+  0b0000000, // 46 '.'  NO DISPLAY
+  0b0000000, // 47 '/'  NO DISPLAY
+  0b1111110, // 48 '0'
+  0b0110000, // 49 '1'
+  0b1101101, // 50 '2'
+  0b1111001, // 51 '3'
+  0b0110011, // 52 '4'
+  0b1011011, // 53 '5'
+  0b1011111, // 54 '6'
+  0b1110000, // 55 '7'
+  0b1111111, // 56 '8'
+  0b1111011, // 57 '9'
+  0b0000000, // 58 ':'  NO DISPLAY
+  0b0000000, // 59 ';'  NO DISPLAY
+  0b0000000, // 60 '<'  NO DISPLAY
+  0b0000000, // 61 '='  NO DISPLAY
+  0b0000000, // 62 '>'  NO DISPLAY
+  0b0000000, // 63 '?'  NO DISPLAY
+  0b0000000, // 64 '@'  NO DISPLAY
+  0b1110111, // 65 'A'
+  0b0011111, // 66 'B'
+  0b1001110, // 67 'C'
+  0b0111101, // 68 'D'
+  0b1001111, // 69 'E'
+  0b1000111, // 70 'F'
+  0b1011110, // 71 'G'
+  0b0110111, // 72 'H'
+  0b0110000, // 73 'I'
+  0b0111000, // 74 'J'
+  0b0000000, // 75 'K'  NO DISPLAY
+  0b0001110, // 76 'L'
+  0b0000000, // 77 'M'  NO DISPLAY
+  0b0010101, // 78 'N'
+  0b1111110, // 79 'O'
+  0b1101111, // 80 'P'
+  0b1110011, // 81 'Q'
+  0b0000101, // 82 'R'
+  0b1011011, // 83 'S'
+  0b0001111, // 84 'T'
+  0b0111110, // 85 'U'
+  0b0000000, // 86 'V'  NO DISPLAY
+  0b0000000, // 87 'W'  NO DISPLAY
+  0b0000000, // 88 'X'  NO DISPLAY
+  0b0111011, // 89 'Y'
+  0b0000000, // 90 'Z'  NO DISPLAY
+  0b1001110, // 91 '['
+  0b0000000, // 92 '\'  NO DISPLAY
+  0b1111000, // 93 ']'
+  0b0000000, // 94 '^'  NO DISPLAY
+  0b0001000, // 95 '_'
+  0b0000010, // 96 '`'
+  0b1110111, // 97 'a' SAME AS CAP
+  0b0011111, // 98 'b' SAME AS CAP
+  0b0001101, // 99 'c'
+  0b0111101, // 100 'd' SAME AS CAP
+  0b1101111, // 101 'e'
+  0b1000111, // 102 'f' SAME AS CAP
+  0b1011110, // 103 'g' SAME AS CAP
+  0b0010111, // 104 'h'
+  0b0010000, // 105 'i'
+  0b0111000, // 106 'j' SAME AS CAP
+  0b0000000, // 107 'k'  NO DISPLAY
+  0b0110000, // 108 'l'
+  0b0000000, // 109 'm'  NO DISPLAY
+  0b0010101, // 110 'n' SAME AS CAP
+  0b0011101, // 111 'o'
+  0b1100111, // 112 'p' SAME AS CAP
+  0b1110011, // 113 'q' SAME AS CAP
+  0b0000101, // 114 'r' SAME AS CAP
+  0b1011011, // 115 's' SAME AS CAP
+  0b0001111, // 116 't' SAME AS CAP
+  0b0011100, // 117 'u'
+  0b0000000, // 118 'b'  NO DISPLAY
+  0b0000000, // 119 'w'  NO DISPLAY
+  0b0000000, // 120 'x'  NO DISPLAY
+  0b0000000, // 121 'y'  NO DISPLAY
+  0b0000000, // 122 'z'  NO DISPLAY
+  0b0000000, // 123 '0b'  NO DISPLAY
+  0b0000000, // 124 '|'  NO DISPLAY
+  0b0000000, // 125 ','  NO DISPLAY
+  0b0000000, // 126 '~'  NO DISPLAY
+  0b0000000, // 127 'DEL'  NO DISPLAY
+ */
+
+#include "SevSeg.h"
+#define CLR 32
+#define NUM_DIGITS 6
+
+//Create an instance of the object.
+SevSeg myDisplay;
+
+//Create global variables
+unsigned long timer;
+int deciSecond = 0;
+
+void clear_display () {
+    char CLR_String[NUM_DIGITS+1] = {CLR,CLR,CLR,CLR,CLR,CLR};
+//    
+//    for(int a = 0; a <= NUM_DIGITS; a++) {
+//    
+//    CLR_String[a] = CLR; //CLR is space
+//    }
+    myDisplay.DisplayString(CLR_String, 0);
+  
+}
+
+void setup()
+{
+
+  int displayType = COMMON_CATHODE; //Your display is either common cathode or common anode
+
+  
+  //This pinout is for a bubble dispaly
+       //Declare what pins are connected to the GND pins (cathodes)
+       int digit1 = 2; //Pin D2
+       int digit2 = 3; //Pin D3
+       int digit3 = 4; //Pin D4
+       int digit4 = 5; //Pin D5
+       int digit5 = 6; //Pin D6
+       int digit6 = 7; //Pin D7
+       
+       //Declare what pins are connected to the segments (anodes)
+       int segA = 8; //Pin D8
+       int segB = 9; //Pin D8
+       int segC = 11; //Pin D11
+       int segD = 13; //Pin D13
+       int segE = 12; //Pin D12
+       int segF = 18; //Pin A4
+       int segG = 19; //Pin A5
+       int segDP= 0; //Pin RX0
+   
+  int numberOfDigits = 6; //Do you have a 6 digit display?
+
+  myDisplay.Begin(displayType, numberOfDigits, digit1, digit2, digit3, digit4, digit5, digit6, segA, segB, segC, segD, segE, segF, segG, segDP);
+  
+  myDisplay.SetBrightness(100); //Set the display to 100% brightness level
+
+  timer = millis();
+}
+
+void loop() {
+  char tempString[7] = {49,'9','7','0','`','S'}; //Used for sprintf
+  
+  for(int i =0;i<100;i++) {
+    myDisplay.DisplayString(tempString, 0); //(numberToDisplay, decimal point location)
+    delay(10);
+  }
+  
+  clear_display ();
+  delay(1000);
+  
+
+  
+//  char tempString[7] = {'T','h','i','s','I','s'}; //Used for sprintf
+//  
+//  for(int i =0;i<100;i++) {
+//    myDisplay.DisplayString(tempString, 0); //(numberToDisplay, decimal point location)
+//    delay(10);
+//  }
+//  
+//  for(int i =0;i<100;i++) {
+//    
+//    tempString[0] = 16; //16 is space
+//    tempString[1] = 'T';
+//    tempString[2] = 'H';
+//    tempString[3] = 'E';
+//    tempString[4] = 16;
+//    tempString[5] = 16;
+//    
+//    myDisplay.DisplayString(tempString, 0);
+//    
+//    delay(10);
+//  }
+//   for(int i =0;i<100;i++) {
+//    tempString[0] = 'R'; 
+//    tempString[1] = 'E';
+//    tempString[2] = 'P';
+//    tempString[3] = 'A';
+//    tempString[4] = 'I';
+//    tempString[5] = 'R';
+//    
+//    myDisplay.DisplayString(tempString, 0);
+//    
+//    delay(6);
+//  }
+//    for(int i =0;i<100;i++) {
+//      
+//      tempString[0] = 16; //16 is blank
+//      tempString[1] = 'C';
+//      tempString[2] = 'A';
+//      tempString[3] = 'F';
+//      tempString[4] = 'E';
+//      tempString[5] = 16;
+//      
+//      myDisplay.DisplayString(tempString, 0);
+//      
+//      delay(6);
+//    }
+//    delay(5);
+
+}
+
